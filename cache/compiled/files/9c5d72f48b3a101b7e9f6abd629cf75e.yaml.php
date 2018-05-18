13cf37655ad7092107ce767f0bca7ec1bdf6b8ea
root <s.r.alamalhoda@gmail.com>
2018-05-18 16:56:39 +0430

    delete everything

--- cache/compiled/files/9c5d72f48b3a101b7e9f6abd629cf75e.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Applications/MAMP/htdocs/noorvadanesh2/user/config/streams.yaml',
-    'modified' => 1525923448,
-    'data' => [
-        
-    ]
-];
--- user/pages/01.Home/04._1news2/1news.md
+++
@@ -1,7 +0,0 @@
----
-title: 'کلاس های تفسیر قرآن کریم'
-background_image: bg22.jpg
-overlay_background_color: 'rgba(126, 163, 222, 1.00)'
-overlay_opacity: '0.5'
----
-
